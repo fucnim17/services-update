3a778facdbf0cf115e90a09aca4d6d33b6d38961
fucnim17 <34504819+fucnim17@users.noreply.github.com>
2025-03-25 18:26:18 +0100

    added photoprism and paperless update

--- services-update.sh
+++ services-update.sh
@@ -5,13 +5,14 @@
 # any later version.
 # See LICENSE file for more details.
 # -----------------------------------------------------------------------------
-# Script to update Jellyfin, Ghostfolio and backup Paperless
+# Script to update Jellyfin, Ghostfolio, Paperless, PhotoPrism and backup Paperless
 # -----------------------------------------------------------------------------
 
 # Variables
 GHOSTFOLIO_COMPOSE_FILE="/root/ghostfolio/docker/docker-compose.yml"
 JELLYFIN_COMPOSE_FILE="/root/jellyfin/docker-compose.yml"
 PAPERLESS_DIRECTORY="/root/paperless-ngx/docker/compose"
+PAPERLESS_COMPOSE_FILE="${PAPERLESS_DIRECTORY}/docker-compose.yml"
 LOG_FILE="/root/services-update/services-update.log"
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 ORIGINAL_DIR=$(pwd)
@@ -64,11 +65,51 @@ docker update --restart unless-stopped gf-redis || error "Docker Update for gf-r
 
 log "Ghostfolio Update completed."
 
-# 3. Docker Image Prune
+# 3. Paperless Update
+log "Starting Paperless Update..."
+
+# 3.1 Change to Paperless directory
+cd "$PAPERLESS_DIRECTORY" || error "Could not change to Paperless directory!"
+
+# 3.2 Stop Paperless
+log "Stopping Paperless services..."
+docker compose down || error "Failed to stop Paperless services!"
+
+# 3.3 Pull latest Paperless images
+log "Pulling latest Paperless Docker images..."
+docker compose pull || error "Failed to pull latest Paperless Docker images!"
+
+# 3.4 Start Paperless services
+log "Starting Paperless services..."
+docker compose up -d || error "Failed to start Paperless services!"
+
+# 3.5 Return to original directory
+cd "$ORIGINAL_DIR" || error "Could not change back to original directory!"
+
+log "Paperless Update completed."
+
+# 4. PhotoPrism Update
+log "Starting PhotoPrism Update..."
+
+# 4.1 Pull latest PhotoPrism image
+log "Pulling latest PhotoPrism image..."
+podman pull docker.io/photoprism/photoprism:latest || error "Failed to pull latest PhotoPrism image!"
+
+# 4.2 Restart PhotoPrism service
+log "Restarting PhotoPrism service..."
+systemctl restart pod-photoprism.service || error "Failed to restart PhotoPrism service!"
+
+log "PhotoPrism Update completed."
+
+# 5. Docker Image Prune
 log "Removing unused Docker Images..."
 docker image prune -a -f || log "Docker Image Prune failed (no problem if no images were removed)."
 
-# 4. Paperless Backup
+# 6. Podman Image Prune
+log "Removing unused Podman Images..."
+podman image prune -a -f || log "Podman Image Prune failed (no problem if no images were removed)."
+
+# 7. Paperless Backup
 log "Starting Paperless Backup..."
 
 cd "$PAPERLESS_DIRECTORY" || error "Could not change to Paperless directory!"
